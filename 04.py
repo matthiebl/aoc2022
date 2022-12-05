bf6ccbb91807a8e6e3c61c949154ccb47b953769
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2022-12-05 20:57:42 +1000

    Refine day 4

--- 04.py
+++ 04.py
@@ -1,39 +1,22 @@
 #!/usr/bin/env python3
 
 from sys import stdin
+import re
 
-def part1(bags: list) -> int:
-    return None
+def part1(pairs: list[tuple]) -> int:
+    return sum((la <= ra and rb <= lb) or (ra <= la and lb <= rb) for la, lb, ra, rb in pairs)
 
-def part2(groupedBags: list) -> int:
-    return None
+def part2(pairs: list[tuple]) -> int:
+    return sum((la <= ra <= lb) or (ra <= la <= rb) for la, lb, ra, rb in pairs)
 
 def main():
     print('Day 04')
 
     data = stdin.read().split('\n')
-    pairs = [tuple(pair.split(',')) for pair in data]
+    pairs = [tuple(map(int, re.split(r',|-', pair))) for pair in data]
 
-    total = 0
-    for l, r in pairs:
-        la, lb = l.split('-')
-        ra, rb = r.split('-')
-        la, lb, ra, rb = int(la), int(lb), int(ra), int(rb)
-        if (la <= ra and rb <= lb) or (ra <= la and lb <= rb):
-            total += 1
-    print(total)
-
-    total2 = 0
-    for l, r in pairs:
-        la, lb = l.split('-')
-        ra, rb = r.split('-')
-        la, lb, ra, rb = int(la), int(lb), int(ra), int(rb)
-        if (la <= ra <= lb) or (ra <= la <= rb):
-            total2 += 1
-    print(total2)
-    # print(f'{part1(bags)=}')
-    # print(f'{part2(groupedBags)=}')
-
+    print(part1(pairs))
+    print(part2(pairs))
 
 if __name__ == '__main__':
     main()
