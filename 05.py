21869793ac78189b6d864302d44bfdd3e5803f2d
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2022-12-05 20:53:07 +1000

    Refine day 5

--- 05.py
+++ 05.py
@@ -3,11 +3,23 @@
 from sys import stdin
 import re
 
-def part1(bags: list) -> int:
-    return None
+def part1(stacks: list[list], moves: list[tuple]) -> str:
+    for move in moves:
+        amount, source, dest = move
+        for _ in range(amount):
+            box = stacks[source-1].pop(0)
+            stacks[dest-1].insert(0, box)
+    return ''.join(stack[0] for stack in stacks)
 
-def part2(groupedBags: list) -> int:
-    return None
+def part2(stacks: list[list], moves: list[tuple]) -> int:
+    for move in moves:
+        amount, source, dest = move
+        boxes = stacks[source-1][:amount]
+        stacks[source-1] = stacks[source-1][amount:]
+
+        boxes.extend(stacks[dest-1])
+        stacks[dest-1] = boxes
+    return ''.join(stack[0] for stack in stacks)
 
 def main():
     print('Day 04')
@@ -17,31 +29,13 @@ def main():
 
     stacks = [list() for i in range(len(stackData[0]) // 4)]
     for line in stackData[:-1]:
-        for i in range(0, len(line) - 1, 4):
-            if line[i+1] != ' ': stacks[i//4].append(line[i+1])
-    
-    moves = [re.split(r'move | from | to ', move)[1:] for move in moves.split('\n')]
+        for i in range(1, len(line), 4):
+            if line[i] != ' ': stacks[i//4].append(line[i])
+    moves = [tuple(map(int, re.split(r'move | from | to ', move)[1:])) for move in moves.split('\n')]
 
-    stacks1 = [stack.copy() for stack in stacks]
-    for move in moves:
-        amount, f, t = move
-        for _ in range(int(amount)):
-            box = stacks1[int(f)-1].pop(0)
-            stacks1[int(t)-1].insert(0, box)
-    print(''.join([stack[0] for stack in stacks1]))
-
-    for move in moves:
-        amount, f, t = move
-        boxes = stacks[int(f)-1][0:int(amount)]
-        stacks[int(f)-1] = stacks[int(f)-1][int(amount):]
-
-        boxes.extend(stacks[int(t)-1])
-        stacks[int(t)-1] = boxes
-    print(''.join([stack[0] for stack in stacks]))
-
-    # print(f'{part1(bags)=}')
-    # print(f'{part2(groupedBags)=}')
-
+    copy = [stack.copy() for stack in stacks]
+    print(part1(copy, moves))
+    print(part2(stacks, moves))
 
 if __name__ == '__main__':
     main()
