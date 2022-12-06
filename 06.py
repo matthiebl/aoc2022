1fd398e7421e872c6092dce796a4e2b4eedaa4c9
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2022-12-06 14:59:01 +1000

    Setup blank day 06

---
+++ 06.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from sys import stdin
+import re
+
+def part1(data: list) -> str:
+    return None
+
+def part2(data: list) -> int:
+    return None
+
+def main():
+    print('Day 06')
+
+    data = stdin.read().split('\n')
+
+if __name__ == '__main__':
+    main()
