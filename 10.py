e9806beef60cc001800e7fa70a9ac0b2a49b0cf7
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2022-12-10 18:40:45 +1000

    Complete day 10

--- 10.py
+++ 10.py
@@ -1,63 +1,40 @@
 #!/usr/bin/env python3
 
 from sys import stdin
-import re
 
 
 def main():
-    print('Day 09')
+    print('Day 10')
 
-    moves = [(move[0], int(move[2:])) for move in stdin.read().split('\n')]
+    raw = stdin.read().split('\n')
 
-    D = {'R': (1, 0), 'L': (-1, 0), 'U': (0, -1), 'D': (0, 1)}
-    hp = (0, 0)
-    h = (0, 0)
-    t = (0, 0)
-    P = set()
+    instructions: list[str] = []
+    for ins in raw:
+        if ins != 'noop':
+            instructions.append('noop')
+        instructions.append(ins)
 
-    def addCoord(a: tuple, b: tuple) -> tuple:
-        return (a[0] + b[0], a[1] + b[1])
-    
-    def dist(a: tuple, b: tuple) -> int:
-        return max(abs(a[0] - b[0]), abs(a[1] - b[1]))
-    
-    P.add(t)
-    for d, it in moves:
-        for _ in range(it):
-            hp = h
-            h = addCoord(h, D[d])
-            if (dist(h, t) > 1):
-                t = hp
-                P.add(t)
-    print(len(P))
+    X = 1
 
-    # b is chasing after a
-    # get bs new pos
-    def chase(a: tuple, b: tuple) -> tuple:
-        if dist(a, b) <= 1: return b
-        ax, ay = a
-        bx, by = b
-        if ax == bx:
-            return bx, (ay + by) // 2
-        if ay == by:
-            return (ax + bx) // 2, by
-        for d in [(1, 1), (1, -1), (-1, 1), (-1, -1)]:
-            m = addCoord(b, d)
-            if dist(a, m) <= 1:
-                return m
-        
+    p1 = 0
 
-    R = [(0, 4)] * 10 # H 1 2 3 4 5 6 7 8 9
-    P2 = set()
-    P2.add(R[9])
+    p2 = []
+    R = ''
+    for cycle, instruction in enumerate(instructions, start=1):
+        if len(R) == 40:
+            p2.append(R)
+            R = ''
+        currentPixel = len(R) # 0
+        R += '#' if X-1 <= currentPixel <= X+1 else ' '
 
-    for d, it in moves:
-        for _ in range(it):
-            R[0] = addCoord(R[0], D[d])
-            for i in range(1, len(R)):
-                R[i] = chase(R[i-1], R[i])
-            P2.add(R[9])
-    print(len(P2))
+        if (cycle - 20) % 40 == 0:
+            p1 += cycle * X
+        if instruction.startswith('addx'):
+            X += int(instruction[5:])
+    p2.append(R)
+
+    print(p1)
+    print('\n'.join(p2))
 
 if __name__ == '__main__':
     main()
