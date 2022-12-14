71e14f6876402e342e2f57390dd01911889b0304
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2022-12-15 09:52:37 +1100

    Update day 4

--- 04.py
+++ 04.py
@@ -1,26 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.10
 
-from sys import stdin
-import re
-
-
-def part1(pairs: list[tuple]) -> int:
-    return sum((x1 <= y1 and y2 <= x2) or (y1 <= x1 and x2 <= y2) for x1, x2, y1, y2 in pairs)
+from sys import argv
+import advent as adv
 
 
-def part2(pairs: list[tuple]) -> int:
-    return sum((x1 <= y1 <= x2) or (y1 <= x1 <= y2) for x1, x2, y1, y2 in pairs)
-
-
-def main():
+def main(file: str) -> None:
     print('Day 04')
 
-    data = stdin.read().split('\n')
-    pairs = [tuple(map(int, re.split(r',|-', pair))) for pair in data]
+    pairs = adv.input_as_lines(file, map=lambda s: adv.double_sep(s, ',', '-', map=int, group=tuple))
 
-    print(part1(pairs))
-    print(part2(pairs))
+    p1 = sum((x1 <= y1 and y2 <= x2) or (y1 <= x1 and x2 <= y2) for (x1, x2), (y1, y2) in pairs)
+    print(p1)
+
+    p2 = sum((x1 <= y1 <= x2) or (y1 <= x1 <= y2) for (x1, x2), (y1, y2) in pairs)
+    print(p2)
 
 
 if __name__ == '__main__':
-    main()
+    file = argv[1] if len(argv) >= 2 else '04.in'
+    main(file)
