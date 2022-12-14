908e5e1116740f65a44fd1fb6813c458ad5908f7
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2022-12-14 20:34:53 +1100

    Add part 2

--- 14.py
+++ 14.py
@@ -3,11 +3,6 @@
 from sys import stdin
 
 
-def showCave(cave: list[list[str]]) -> None:
-    for row in cave:
-        print(''.join(row).replace('.', '.'))
-
-
 def getMove(x, y, cave):
     if y == len(cave) - 1:
         return None
@@ -26,16 +21,12 @@ def main():
     paths = [[(int(coord.split(',')[0]), int(coord.split(',')[1]))
               for coord in path.split(' -> ')] for path in stdin.read().split('\n')]
 
-    minX, minY = 500, 0
-    maxX, maxY = 500, 0
+    maxY = 0
     for path in paths:
-        for x, y in path:
-            minX = min(x, minX)
-            maxX = max(x, maxX)
-            minY = min(y, minY)
+        for _, y in path:
             maxY = max(y, maxY)
 
-    cave = [['.'] * (maxX + 2 - (minX - 2)) for _ in range(maxY + 2)]
+    cave = [['.'] * (2 * maxY + 5) for _ in range(maxY + 2)]
 
     for path in paths:
         x1, y1 = path.pop(0)
@@ -43,26 +34,28 @@ def main():
             x2, y2 = path.pop(0)
             for x in range(x1, x2 + (1 if x1 <= x2 else -1), 1 if x1 <= x2 else -1):
                 for y in range(y1, y2 + (1 if y1 <= y2 else -1), 1 if y1 <= y2 else -1):
-                    cave[y][x - (minX - 2)] = '#'
-
+                    cave[y][x-500+maxY+2] = '#'
             x1, y1 = x2, y2
 
-    cave[0][500 - (minX - 2)] = '+'
+    cave[0][500-500+maxY+2] = '+'
 
-    p1 = 0
+    placed = 0
+    p1 = False
     while True:
-        sandx, sandy = 500 - (minX - 2), 0
+        sandx, sandy = 500-500+maxY+2, 0
 
         while getMove(sandx, sandy, cave) is not None:
             sandx, sandy = getMove(sandx, sandy, cave)
 
-        if sandy == maxY + 1:
-            break
+        if not p1 and sandy == maxY + 1:
+            print(placed)
+            p1 = True
         cave[sandy][sandx] = 'o'
-        p1 += 1
+        placed += 1
+        if sandy == 0:
+            break
 
-    showCave(cave)
-    print(p1)
+    print(placed)
 
 
 if __name__ == '__main__':
