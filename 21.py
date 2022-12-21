31980b0d21b0ef02d5a871c855b693ea5fd60992
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2022-12-21 18:24:36 +1100

    Improve part 2 of day 21

--- 21.py
+++ 21.py
@@ -35,18 +35,53 @@ def main(file: str) -> None:
     p1 = determine('root')
     print(p1)
 
-    # This is dirty and solved by hand foy my specific input.
-    jobs['humn'] = 3916491093817
-    for attempt in range(3916491093800, 3916491094000):
-        jobs['humn'] = attempt
+    # This is dirty and solved by hand foy my specific input. 3916491093817
+    # for attempt in range(3916491093800, 3916491094000):
+    #     jobs['humn'] = attempt
 
-        a, _, b = jobs['root']
-        left = determine(a)
-        right = determine(b)
+    #     a, _, b = jobs['root']
+    #     left = determine(a)
+    #     right = determine(b)
 
-        if left == right:
-            print(attempt)
+    #     if left == right:
+    #         print(attempt)
+    #         break
+
+    a, _, b = jobs['root']
+
+    jobs['humn'] = 1000
+    direction = determine(a)
+    jobs['humn'] = 100000
+    direction -= determine(a)
+
+    p2 = p1 * 10
+
+    # Use binary search to find the correct human value
+    def binary(lo, hi, target, direction):
+        mid = (lo + hi) // 2
+        jobs['humn'] = mid
+        current = determine(a)
+
+        if current == target:
+            return mid
+        if current < target:
+            if direction < 0: return binary(mid, hi, target, direction)
+            return binary(lo, mid, target, direction)
+        if current > target:
+            if direction < 0: return binary(lo, mid, target, direction)
+            return binary(mid, hi, target, direction)
+
+    p2 = binary(0, p2, determine(b), direction)
+
+    # Find the minimal value of p2 to satisfy the equality check
+    while True:
+        jobs['humn'] = p2 - 1
+        if determine(a) == determine(b):
+            p2 -= 1
+        else:
             break
+
+    print(p2)
 
 
 if __name__ == '__main__':
